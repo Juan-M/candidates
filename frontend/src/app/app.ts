91dd601f89e255a324ddde88541cb3652c2a86c6
Juan M. García González <juandoit@gmail.com>
2025-10-20 08:38:33 +0200

    feat: start implementing the frontend

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,9 +1,21 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatListModule } from '@angular/material/list';
+import { CandidateUploadComponent } from 'src/components/candidateUpload.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    MatButtonModule,
+    MatCardModule,
+    CandidateUploadComponent,
+    MatListModule,
+    MatDividerModule
+  ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
